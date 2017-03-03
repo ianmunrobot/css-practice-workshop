46d0d82351f27750660a11b5c39b7d35c124b00f
Aliza Simons <ianmunrobot@gmail.com>
2017-03-03 16:02:10 -0500

    added CORS support
    required CORS for serving/parsing actual CSS rules

--- index.js
+++ index.js
@@ -2,17 +2,34 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const { findCSSSelector } = require('./utils/scraper')
+const volleyball = require('volleyball');
 
+const { findCSSSelector } = require('./utils/scraper');
 const app = express();
-const PORT = 1337;
+const {PORT} = require('./_server.config');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/css/', (req, res, next) => {
+// required to serve files via CORS to karma test server
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://0.0.0.0:9876');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
+// logging (dev)
+app.use(volleyball)
+
+// find and return css text by path string
+// if selector query string is given, find specific selector;
+app.get('/css', (req, res, next) => {
   fs.readFile(`css/${req.query.path}`, (err, data) => {
-    if (err) res.send(err);
+    if (err) {
+      res.send(err);
+    }
     else {
       data = data.toString();
       if (req.query.selector) {
@@ -22,7 +39,7 @@ app.get('/css/', (req, res, next) => {
           res.send('not found');
         } else {
           let endIndex = startIndex + data.slice(startIndex).indexOf('}') + 1;
-          let response = data.slice(startIndex, endIndex);
+          let response = data.slice(startIndex + req.query.selector.length, endIndex);
           res.send(response);
         }
       } else {
