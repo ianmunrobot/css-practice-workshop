5f0f3d1e100a07b718b346192ea8b33d9bb9fd0d
Aliza Simons <ianmunrobot@gmail.com>
2017-03-10 17:46:40 -0500

    refactored border secrets

---
+++ css/border/border.secrets.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+
+module.exports = {
+
+  borderStyle: bgDiv => {
+    let borderStyle = bgDiv.getRawStyle('border-style');
+    let borderWidth = bgDiv.getRawStyle('border-width');
+    let borderColor = bgDiv.getRawStyle('border-color');
+    expect(borderColor).to.be.equal('rgb(0, 0, 255)');
+    expect(borderWidth).to.be.equal('2px');
+    expect(borderStyle).to.be.equal('dotted');
+  },
+
+  oneLineStyle: style => {
+    expect(style).to.not.include('border-style');
+    expect(style).to.not.include('border-width');
+    expect(style).to.not.include('border-color');
+    expect(style).to.include('border:')
+  }
+
+}
--- css/border/border.test.js
+++ css/border/border.test.js
@@ -1,7 +1,7 @@
-const expect = require('chai').expect;
 const quixote = require('quixote');
 const axios = require('axios');
 
+const secrets = require('./border.secrets');
 const findCSSSelector = require('../css.utils').findCSSSelector;
 
 describe('Border', () => {
@@ -31,24 +31,14 @@ describe('Border', () => {
     });
 
     it('has the correct styling', () => {
-      let borderStyle = bgDiv.getRawStyle('border-style');
-      let borderWidth = bgDiv.getRawStyle('border-width');
-      let borderColor = bgDiv.getRawStyle('border-color');
-      expect(borderColor).to.be.equal('rgb(0, 0, 255)');
-      expect(borderWidth).to.be.equal('2px');
-      expect(borderStyle).to.be.equal('dotted');
+      secrets.borderStyle(bgDiv)
     });
 
     it('uses the shorthand one-line border style', () => {
       return axios.get(`/base/css/border/border.css`)
       .then(res => res.data)
       .then(text => findCSSSelector(text, '.single-line-border'))
-        .then(style => {
-          expect(style).to.not.include('border-style');
-          expect(style).to.not.include('border-width');
-          expect(style).to.not.include('border-color');
-          expect(style).to.include('border:')
-        })
+        .then(secrets.oneLineStyle)
     });
 
   });
