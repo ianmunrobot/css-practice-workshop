9332483774b69c7f022d131b32f86a04e27e5860
Ian Munro <ianmunrobot@gmail.com>
2017-04-04 15:10:21 -0400

    adds additional navbar tests

--- css/navbar/navbar.test.js
+++ css/navbar/navbar.test.js
@@ -22,14 +22,30 @@ describe('Navbar', () => {
 
   describe('navbar-big', () => {
     let navbar,
-        navbarItems;
+        navbarItems,
+        navbarUl;
 
     beforeEach('grab navbar', () => {
       navbar = frame.get('#navbar-big');
+      navbarUl = frame.get('#navbar-big ul');
       navbarItems = frame.getAll('.navbar-big li', 'navbar-big <li>s');
     });
 
+    it('displays navbar ul with gray background', () => {
+      let color = navbarUl.getRawStyle('background-color');
+      let color2 = navbar.getRawStyle('background-color');
+      expect([color, color2]).to.include('rgb(128, 128, 128)')
+    });
+
+    it('displays list items with white text', () => {
+      for (let i = 0; i < navbarItems.length; i++) {
+        let liColor = navbarItems.at(i).getRawStyle('color');
+        expect(liColor).to.be.equal('rgb(0, 0, 0)')
+      }
+    });
+
     it('displays list items in a row', () => {
+      // assert that all navbar li tops match
       for (let i = 0; i < navbarItems.length; i++) {
         let current = navbarItems.at(i);
         current.assert({
@@ -37,6 +53,7 @@ describe('Navbar', () => {
         });
       }
     });
+
 
   });
 
