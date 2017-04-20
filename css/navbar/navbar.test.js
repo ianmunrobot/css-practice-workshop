29f7c5c81d8a34e314b00997764eb8e795fea8c0
Ian Munro <ianmunrobot@gmail.com>
2017-04-20 16:13:12 -0400

    adds navbar breadcrumb tests

--- css/navbar/navbar.test.js
+++ css/navbar/navbar.test.js
@@ -1,7 +1,9 @@
 const quixote = require('quixote');
 const expect = require('chai').expect;
+const axios = require('axios')
+const findCSSSelector = require('../css.utils').findCSSSelector;
 
-describe('Navbar', () => {
+describe.only('Navbar', () => {
   let frame;
 
   before(done => {
@@ -26,7 +28,7 @@ describe('Navbar', () => {
         navbarUl,
         navbarAs;
 
-    beforeEach('grab navbar', () => {
+    beforeEach('grab navbar and navbar items', () => {
       navbar = frame.get('#navbar-big');
       navbarUl = frame.get('#navbar-big ul');
       navbarItems = frame.getAll('.navbar-big li', '.navbar-big <li>s');
@@ -46,6 +48,29 @@ describe('Navbar', () => {
       }
     });
 
+    // in this case, we can't safely simulate click/mouseover events, so we parse the css file itself
+    it('displays list items with bright green background on mouseover', () => {
+      return axios.get(`/base/css/navbar/navbar.css`)
+        .then(res => res.data)
+        .then(text => findCSSSelector(text, '.navbar-big li:hover'))
+        .then(style => {
+          expect(style).to.be.ok;
+          let styleTest = /background-color:\s#32cd32/.test(style)
+          expect(styleTest).to.be.true;
+        });
+    });
+
+    it('displays list items with gray background on select', () => {
+      return axios.get(`/base/css/navbar/navbar.css`)
+        .then(res => res.data)
+        .then(text => findCSSSelector(text, '.navbar-big li:active'))
+        .then(style => {
+          expect(style).to.be.ok;
+          let styleTest = /background-color:\s#1e1e1e/.test(style)
+          expect(styleTest).to.be.true;
+        });
+    });
+
     it('displays list items in a row', () => {
       // assert that all navbar li tops match
       for (let i = 0; i < navbarItems.length(); i++) {
@@ -53,6 +78,50 @@ describe('Navbar', () => {
         current.assert({
           top: navbar.top
         });
+      }
+    });
+
+  });
+
+  describe('navbar-breadcrumb', () => {
+    let navbar,
+        navbarItems,
+        navbarUl,
+        navbarAs;
+
+    beforeEach('grab navbar and navbar items', () => {
+      navbar = frame.get('#navbar-breadcrumb');
+      navbarUl = frame.get('#navbar-breadcrumb ul');
+      navbarItems = frame.getAll('.navbar-breadcrumb li', '.navbar-breadcrumb <li>s');
+      navbarAs = frame.getAll('.navbar-breadcrumb a', '.navbar-breadcrumb <a>s')
+    });
+
+    it('displays navbar with the correct bg color', () => {
+      let color = navbarUl.getRawStyle('background-color');
+      let color2 = navbar.getRawStyle('background-color');
+      expect([color, color2]).to.include('rgb(16, 163, 167)');
+    });
+
+    it('displays list items in a row', () => {
+      // assert that all navbar li tops match
+      for (let i = 0; i < navbarItems.length(); i++) {
+        let current = navbarItems.at(i);
+        current.assert({
+          top: navbar.top.plus(10)
+        });
+      }
+    });
+
+    it('displays a "/" between each li (but not at the end)', () => {
+      let current = navbarItems.at(1);
+      for (let i = 0; i < navbarItems.length(); i++) {
+        let current = navbarItems.at(i);
+        let content = window.getComputedStyle(current.toDomElement(), ':before').getPropertyValue('content');
+        if (i === 0) {
+          expect(content).to.be.equal('');
+        } else {
+          expect(content).to.be.equal('"/"');
+        }
       }
     });
 
